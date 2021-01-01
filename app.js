9dae60143fb91f4dadd3d112c6cd4b5463cb6fcf
Mubahsir <smubashir655@gmail.com>
2021-01-01 12:01:24 +0500

    assignment 12

---
+++ app.js
@@ -0,0 +1,33 @@
+//..............Chepter # 1..............//
+
+ //.......... Question 1..........//
+
+// alert("Welcome to my website")
+
+ //.......... Question 2..........//
+
+//  alert("Error! Please enter a valid passowrd.")
+
+ //.......... Question 3..........//
+
+//  alert("Welcome to JS Land... \nHappy Coding!")
+
+ //.......... Question 4..........//
+
+//  alert("Welcome to JS Land...")
+//  alert("Happy Coding! \nPrevent this page from creating additional dialogs.")
+
+ //.......... Question 5..........//
+
+//  console.log("Hello... I can run JS trough my web browser's console")
+
+ //.......... Question 6..........//
+
+ //Check HTML file
+
+ //.......... Question 7..........//
+
+//Check HTML file
+
+
+//..............Chepter # 2..............//
